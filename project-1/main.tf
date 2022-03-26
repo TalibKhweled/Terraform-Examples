aba9fe6ea5704142c1db40b72ba94bead507519a
TalibKhweled <TalibKhweled@gmail.com>
2022-03-26 01:07:27 -0700

    simple web server

---
+++ project-1/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-04505e74c0741db8d"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Fuck you Jeff" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+    tags = {
+        Name = "terraform-example"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
