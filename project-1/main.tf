17cb28ff6f975c2a5df8bade009ae8b7bf90dd71
TalibKhweled <TalibKhweled@gmail.com>
2022-04-11 00:00:35 -0600

    Added variables for server port

--- project-1/main.tf
+++ project-1/main.tf
@@ -2,16 +2,21 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-04505e74c0741db8d"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+variable "server_port" {
+    description = "Port number for web server to use for http reqs"
+    type = number
+    default = 8080
+}
 
-    user_data = <<-EOF
-                #!/bin/bash
-                echo "Fuck you Jeff" > index.html
-                nohup busybox httpd -f -p 8080 &
-                EOF
+resource "aws_instance" "example" {
+    ami                    = "ami-04505e74c0741db8d"
+    instance_type          = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+    user_data              = <<-EOF
+                             #!/bin/bash
+                             echo "Fuck you Jeff" > index.html
+                             nohup busybox httpd -f -p ${var.server_port} &
+                             EOF
 
     tags = {
         Name = "terraform-example"
@@ -21,9 +26,9 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
     ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
+        from_port   = var.server_port
+        to_port     = var.server_port
+        protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
